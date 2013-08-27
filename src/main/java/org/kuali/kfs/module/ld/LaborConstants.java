bbefac15c98e5ea3f8aaeaca618d33dfdeaa0a4e
lshyu <lshyu@vivantech.com>
2013-08-27 14:37:13 -0400

    KFSPTS-1627 : changed to permission check.  use paired invalid transfer account type parameter list.  put error message to foundation's applicationresource.properties

--- src/main/java/org/kuali/kfs/module/ld/LaborConstants.java
+++ src/main/java/org/kuali/kfs/module/ld/LaborConstants.java
@@ -302,6 +302,6 @@ public class LaborConstants {
         public static final String GENERATE_FRINGE_BENEFIT_ENCUMBRANCE_PARAMETER = "GENERATE_FRINGE_BENEFIT_ENCUMBRANCE";
     }
     
-    public static final String LABOR_DISTRIBUTION_MANAGER = "Labor Distribution Manager (cu)";
+    public static final String LD_ACCOUNT_TYPE_TRANSFER_EXCEPTION = "LD Account Type Transfer Exception";
 
 }
