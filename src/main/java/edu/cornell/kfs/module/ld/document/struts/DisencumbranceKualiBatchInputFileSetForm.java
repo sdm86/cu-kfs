26f575f985ce99aeb953d0f768adb392951c41de
danielabalmus <db483@cornell.edu>
2013-10-07 14:40:59 +0100

    Revert "KFSPTS-2400 Add create disencumbrance file solution that allows user to select input encumbrance and recon file from local computer"
    This reverts commit 315c7d0981661057e97d518cea994019eaa0081c.

--- src/main/java/edu/cornell/kfs/module/ld/document/struts/DisencumbranceKualiBatchInputFileSetForm.java
+++
@@ -1,7 +0,0 @@
-package edu.cornell.kfs.module.ld.document.struts;
-
-import org.kuali.kfs.sys.web.struts.KualiBatchInputFileSetForm;
-
-public class DisencumbranceKualiBatchInputFileSetForm extends KualiBatchInputFileSetForm {
-
-}
