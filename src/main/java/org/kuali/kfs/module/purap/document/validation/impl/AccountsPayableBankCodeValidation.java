9dc64a02928620d42898b224883c859db272aa9d
lshyu <lshyu@vivantech.com>
2013-10-22 14:18:23 -0400

    KFSPTS1891,KFSPTS-2747 : merge to develop

--- src/main/java/org/kuali/kfs/module/purap/document/validation/impl/AccountsPayableBankCodeValidation.java
+++ src/main/java/org/kuali/kfs/module/purap/document/validation/impl/AccountsPayableBankCodeValidation.java
@@ -41,9 +41,9 @@ public class AccountsPayableBankCodeValidation extends GenericValidation {
 
         // check if one of the extended UA documents, if so, take the payment method into account, otherwise, revert to baseline behavior
         boolean isValid = true;
-        if ( apDocument instanceof PaymentRequestDocument ||  apDocument instanceof VendorCreditMemoDocument) {
+        if ( apDocument instanceof PaymentRequestDocument) {
         	if (StringUtils.isNotBlank(apDocument.getBankCode())) {
-        		// PREQ & CM bank code is not required
+        		// PREQ bank code is not required
                 isValid = BankCodeValidation.validate(apDocument.getBankCode(), "document." + PurapPropertyConstants.BANK_CODE, ((PaymentRequestDocument)apDocument).getPaymentMethodCode(), false, true);            
                 if ( isValid ) {
                     if ( !(event instanceof AttributedRouteDocumentEvent) &&  StringUtils.isNotBlank(apDocument.getBankCode())
@@ -52,6 +52,18 @@ public class AccountsPayableBankCodeValidation extends GenericValidation {
                         apDocument.setBankCode(null);                
                     }
                 }
+        	}
+        }  else if ( apDocument instanceof VendorCreditMemoDocument ) {
+        	if (StringUtils.isNotBlank(apDocument.getBankCode())) {
+        	     isValid = BankCodeValidation.validate(apDocument.getBankCode(), "document." + PurapPropertyConstants.BANK_CODE,  ((VendorCreditMemoDocument)apDocument).getPaymentMethodCode(), false, true);                        
+        	     if ( isValid ) {
+                // clear out the bank code on the document if not needed (per the message set by the call above)
+        	          if ( StringUtils.isNotBlank(apDocument.getBankCode())
+        	        		        && !BankCodeValidation.doesBankCodeNeedToBePopulated(((VendorCreditMemoDocument)apDocument).getPaymentMethodCode()) ) {
+        	        	    apDocument.setBank(null);
+        	        	    apDocument.setBankCode(null);                
+        	        }
+               }
         	}
         } else {
             isValid = BankCodeValidation.validate(apDocument.getBankCode(), "document." + PurapPropertyConstants.BANK_CODE, false, true);
