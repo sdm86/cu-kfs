8809c2154b552f443aef0961027f46ee85611d47
danielabalmus <db483@cornell.edu>
2013-10-22 14:53:18 +0100

    Revert "KSPTS-2719 Set the cancel flag to Yes on related payment details when processing cancelled CR checks"
    This reverts commit a54df1f45faa31b7ed132a5d5e317a59a3837271.

--- src/main/java/com/rsmart/kuali/kfs/cr/batch/GlTransactionStep.java
+++ src/main/java/com/rsmart/kuali/kfs/cr/batch/GlTransactionStep.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.kuali.kfs.pdp.businessobject.PaymentDetail;
 import org.kuali.kfs.pdp.businessobject.PaymentGroup;
 import org.kuali.kfs.pdp.businessobject.PaymentStatus;
 import org.kuali.kfs.pdp.service.PendingTransactionService;
@@ -158,12 +157,6 @@ public class GlTransactionStep extends AbstractStep {
                         }
                         paymentGroup.setLastUpdate(new Timestamp(lastUpdate.getTime()));
                         businessObjectService.save(paymentGroup);
-                        
-                        //update cancel flag on payment details
-                        for (PaymentDetail paymentDetail : paymentGroup.getPaymentDetails()) {
-                            paymentDetail.setPrimaryCancelledPayment(Boolean.TRUE);
-                            businessObjectService.save(paymentDetail);
-                        }
                     
                         // Update status
                         cr.setGlTransIndicator(Boolean.TRUE);
