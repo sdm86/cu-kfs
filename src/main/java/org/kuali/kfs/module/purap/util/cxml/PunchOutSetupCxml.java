c3b4f3ea82ca48103744cecb9463d49f5e8184c9
sbower <shawn.bower@gmail.com>
2014-04-03 16:21:38 -0400

    Revert "Merge branch 'KFSPTS-3191' into develop"
    This reverts commit 66ed0ce95dbf6c356640a2d310e165657b27118c, reversing
    changes made to fbba98544d24fb2094f5634d947074ea0f5878e0.

--- src/main/java/org/kuali/kfs/module/purap/util/cxml/PunchOutSetupCxml.java
+++ src/main/java/org/kuali/kfs/module/purap/util/cxml/PunchOutSetupCxml.java
@@ -22,7 +22,6 @@ package org.kuali.kfs.module.purap.util.cxml;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import org.kuali.kfs.module.purap.PurapConstants;
@@ -32,12 +31,9 @@ import org.kuali.kfs.module.purap.util.PurApDateFormatUtils;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kim.bo.Person;
-import org.kuali.rice.kim.service.KIMServiceLocator;
 import org.kuali.rice.kim.service.RoleManagementService;
 import org.kuali.rice.kns.service.DateTimeService;
 import org.kuali.rice.kns.util.GlobalVariables;
-
-import edu.cornell.kfs.sys.CUKFSConstants;
 
 public class PunchOutSetupCxml {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PunchOutSetupCxml.class);
@@ -106,7 +102,6 @@ public class PunchOutSetupCxml {
     cxml.append("      <Extrinsic name=\"FirstName\">").append(user.getFirstName()).append("</Extrinsic>\n");
     cxml.append("      <Extrinsic name=\"LastName\">").append(user.getLastName()).append("</Extrinsic>\n");
     cxml.append("      <Extrinsic name=\"Role\">").append(getPreAuthValue(user.getPrincipalId())).append("</Extrinsic>\n");
-    cxml.append("      <Extrinsic name=\"Role\">").append(getViewValue(user.getPrincipalId())).append("</Extrinsic>\n");
     cxml.append("      <BrowserFormPost>\n");
     cxml.append("        <URL>").append(b2bInformation.getPunchbackURL()).append("</URL>\n");
     cxml.append("      </BrowserFormPost>\n");
@@ -129,39 +124,19 @@ public class PunchOutSetupCxml {
 
   // KFSPTS-1720
   private String getPreAuthValue(String principalId) {
-      try {
-    	  //Check for special view role first
-    	  if (KIMServiceLocator.getPermissionService().hasPermission(
-              principalId, KFSConstants.ParameterNamespaces.PURCHASING, KFSConstants.SysKimConstants.B2B_SUBMIT_ESHOP_CART_PERMISSION, null))  {
-    		  return "Preauthorized";
-    	  } else {
-        	return "NonPreauthorized";
-    	  }
-        
-      } catch (Exception e) {
-          // incase something goes wrong.  continue to process
-          LOG.info("error from role check " + e.getMessage());
-          return "NonPreauthorized";
-      }
-  }
-  private String getViewValue(String principalId) {
-      try {
-    	  //Check for special view role first
-    	  if (KIMServiceLocator.getPermissionService().hasPermission(
-              	principalId, KFSConstants.ParameterNamespaces.PURCHASING, KFSConstants.SysKimConstants.B2B_SHOPPER_OFFICE_PERMISSION, null))  {
-    		  	return "Office";
-    	  } else if (KIMServiceLocator.getPermissionService().hasPermission(
-	      	  	principalId, KFSConstants.ParameterNamespaces.PURCHASING, KFSConstants.SysKimConstants.B2B_SHOPPER_LAB_PERMISSION, null))  {
-				return "Lab";
-		  } else {
-				return "Unrestricted";
-    	  }
-        
-      } catch (Exception e) {
-          // incase something goes wrong.  continue to process
-          LOG.info("error from role check " + e.getMessage());
-          return "Unrestricted";
-      }
+	  try {
+		List<String> roleIds = new ArrayList<String>();
+		roleIds.add(getRoleManagementService().getRoleIdByName(KFSConstants.ParameterNamespaces.PURCHASING,KFSConstants.SysKimConstants.ESHOP_USER_ROLE_NAME));
+		roleIds.add(getRoleManagementService().getRoleIdByName(KFSConstants.ParameterNamespaces.PURCHASING,KFSConstants.SysKimConstants.ESHOP_SUPER_USER_ROLE_NAME));
+		if (getRoleManagementService().principalHasRole(principalId,roleIds, null)) {
+			return "Preauthorized";
+		}
+		return "NonPreauthorized";
+	  } catch (Exception e) {
+		  // incase something goes wrong.  continue to process
+		  LOG.info("error from role check " + e.getMessage());
+		  return "Preauthorized";
+	  }
   }
   
   private RoleManagementService getRoleManagementService() {
